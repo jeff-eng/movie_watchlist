b72f19f490992d0e68caf7c858d15173f0cbfa5a
jeff-eng <me@jeffeng.com>
2024-03-08 16:56:44 -0800

    Changed class instance variables to match the object from API in order to make it easier to render watchlist class instances from local storage.

--- js/classes.js
+++ js/classes.js
@@ -1,25 +1,25 @@
 class Media {
     constructor(obj) {
-        this.title = obj.Title;
-        this.year = obj.Year;
-        this.rated = obj.Rated;
-        this.released = obj.Released;
-        this.runtime = obj.Runtime;
-        this.genre = obj.Genre;
-        this.director = obj.Director;
-        this.writer = obj.Writer;
-        this.actors = obj.Actors;
-        this.plot = obj.Plot;
-        this.language = obj.Language;
-        this.country = obj.Country;
-        this.awards = obj.Awards;
-        this.poster = obj.Poster;
-        this.ratings = obj.Ratings;
-        this.metascore = obj.Metascore;
+        this.Title = obj.Title;
+        this.Year = obj.Year;
+        this.Rated = obj.Rated;
+        this.Released = obj.Released;
+        this.Runtime = obj.Runtime;
+        this.Genre = obj.Genre;
+        this.Director = obj.Director;
+        this.Writer = obj.Writer;
+        this.Actors = obj.Actors;
+        this.Plot = obj.Plot;
+        this.Language = obj.Language;
+        this.Country = obj.Country;
+        this.Awards = obj.Awards;
+        this.Poster = obj.Poster;
+        this.Ratings = obj.Ratings;
+        this.Metascore = obj.Metascore;
         this.imdbRating = obj.imdbRating;
         this.imdbVotes = obj.imdbVotes;
         this.imdbID = obj.imdbID;
-        this.type = obj.Type;
+        this.Type = obj.Type;
         this.liked = false;
     }
 
@@ -35,22 +35,22 @@ class Media {
         const article = this.#createBasicElement('article', this.imdbID, 'result');
         
         // Use placeholder image for instances where poster is N/A
-        if (this.poster === 'N/A') {
-            this.poster = './../assets/imgholdr-image.png';
+        if (this.Poster === 'N/A') {
+            this.Poster = './../assets/imgholdr-image.png';
         }
 
         article.innerHTML = `
             <picture>
-                <source srcset="${this.poster}" type="image/png">
-                <img class="result__img" src="${this.poster}" alt="${this.title} ${this.type} poster">
+                <source srcset="${this.Poster}" type="image/png">
+                <img class="result__img" src="${this.Poster}" alt="${this.Title} ${this.Type} poster">
             </picture>
-            <h2 class="result__title">${this.title} (<time datetime="${this.year}">${this.year}</time>)</h2>
+            <h2 class="result__title">${this.Title} (<time datetime="${this.Year}">${this.Year}</time>)</h2>
             <button class="result__like-btn" type="button">
                 <i class="${this.liked ? 'fa-solid' : 'fa-regular'} fa-heart" data-imdb-id="${this.imdbID}"></i>
             </button>
-            <span class="result__type">${this.type}</span>
-            <time datetime="PT${this.runtime.split(' ')[0]}M">${this.runtime}</time>
-            <span class="result__genre">${this.genre}</span>
+            <span class="result__type">${this.Type}</span>
+            <time datetime="PT${this.Runtime.split(' ')[0]}M">${this.Runtime}</time>
+            <span class="result__genre">${this.Genre}</span>
             <span class="result__rating">
                 <i class="fa-solid fa-star"></i>
                 <span class="result__rating-value">${this.imdbRating}</span>
@@ -68,8 +68,8 @@ class Media {
         const mediaDivEl = this.#createBasicElement('div', '', 'media');
 
         // Use placeholder image for instances where poster is N/A
-        if (this.poster === 'N/A') {
-            this.poster = './../assets/imgholdr-image.png';
+        if (this.Poster === 'N/A') {
+            this.Poster = './../assets/imgholdr-image.png';
         }
 
         mediaDivEl.innerHTML = `
@@ -77,38 +77,38 @@ class Media {
                 <i class="fa-regular fa-heart"></i>
             </button>
             <picture>
-                <source srcset="${this.poster}" type="image/png">
-                <img class="media__poster" src="${this.poster}" alt="${this.title} ${this.type} poster">
+                <source srcset="${this.Poster}" type="image/png">
+                <img class="media__poster" src="${this.Poster}" alt="${this.Title} ${this.Type} poster">
             </picture>
-            <h2 class="media__title">${this.title}</h2>
-            <time class="media__runtime" datetime="PT${this.runtime.split(' ')[0]}M">${this.runtime}</time>
-            <span class="media__rated">${this.rated}</span>
+            <h2 class="media__title">${this.Title}</h2>
+            <time class="media__runtime" datetime="PT${this.Runtime.split(' ')[0]}M">${this.Runtime}</time>
+            <span class="media__rated">${this.Rated}</span>
             <span class="media__rating">
                 <i class="fa-solid fa-star"></i>
                 <span class="media__rating-value">${this.imdbRating}</span>
             </span>
-            <span class="media__genre">${this.genre}</span>
-            <p class="media__plot">${this.plot}</p>
+            <span class="media__genre">${this.Genre}</span>
+            <p class="media__plot">${this.Plot}</p>
             <dl class="media__info">
                 <div>
                     <dt class="media__term">Year</dt>
-                    <dd class="media__detail">${this.year}</dd>
+                    <dd class="media__detail">${this.Year}</dd>
                 </div>
                 <div>
                     <dt class="media__term">Director(s)</dt>
-                    <dd class="media__detail">${this.director}</dd> 
+                    <dd class="media__detail">${this.Director}</dd> 
                 </div>
                 <div>
                     <dt class="media__term">Writer(s)</dt>
-                    <dd class="media__detail">${this.writer}</dd>
+                    <dd class="media__detail">${this.Writer}</dd>
                 </div>
                 <div>
                     <dt class="media__term">Actor(s)</dt>
-                    <dd class="media__detail">${this.actors}</dd>
+                    <dd class="media__detail">${this.Actors}</dd>
                 </div>
                 <div>
                     <dt class="media__term">Box Office</dt>
-                    <dd class="media__detail">${this.boxOffice ?? 'N/A'}</dd>
+                    <dd class="media__detail">${this.BoxOffice ?? 'N/A'}</dd>
                 </div>
             </dl>
         `;
@@ -120,10 +120,10 @@ class Media {
 class Movie extends Media {
     constructor(obj) {
         super(obj);
-        this.dvd = obj.DVD;
-        this.boxOffice = obj.BoxOffice;
-        this.production = obj.Production;
-        this.website = obj.Website;
+        this.Dvd = obj.DVD;
+        this.BoxOffice = obj.BoxOffice;
+        this.Production = obj.Production;
+        this.Website = obj.Website;
     }
 }
 
@@ -134,4 +134,4 @@ class Series extends Media {
     }
 }
 
-export { Media, Movie, Series};
+export { Media, Movie, Series };
