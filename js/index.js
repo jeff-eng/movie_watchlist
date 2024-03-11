354bb769871b3453a3ce421ba6c9b61431bd70e5
jeff-eng <me@jeffeng.com>
2024-03-10 22:10:23 -0700

    Saved recent search term to variable so user cannot search same term twice consecutively.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const BASE_URL = `http://www.omdbapi.com/?apikey=${apiKey}&`;
 const searchForm = document.getElementById('search__form');
 const searchInput = document.getElementById('search__input');
 
-let currentSearchTerm = '';
+let recentSearchTerm = '';
 let watchlist = setupWatchlist();
 let searchResults = [];
 
@@ -16,23 +16,24 @@ searchInput.addEventListener('input', () => {
     const searchInput = document.getElementById('search__input');
     const searchButton = document.getElementById('search__button');
 
-    searchButton.disabled = searchInput.value.length > 0 ? false : true; 
+    searchButton.disabled = searchInput.value.length > 0 ? false : true;
 });
 
 searchForm.addEventListener('submit', async event => {
     event.preventDefault();
-    console.log('Clicked');
+
     const searchFormData = new FormData(searchForm);
     const searchQuery = searchFormData.get('search-string').toLowerCase();
 
-    if (searchQuery !== currentSearchTerm) {
+    if (searchQuery !== recentSearchTerm) {
         searchResults = await getSearchResults(BASE_URL, searchQuery, watchlist);
     } else return;
 
-    
     if (searchResults.length) {
         const articles = searchResults.map(resultObj => resultObj.createResultHtml());
         document.getElementById('search-results').replaceChildren(...articles);
+        // Save recent search query so user doesn't try to search same term twice in a row
+        recentSearchTerm = searchQuery;
     } else {
         document.getElementById('search-results').innerHTML = '<h2>No results found.</h2>';
     }
@@ -50,7 +51,6 @@ document.getElementById('search-results').addEventListener('click', event => {
         const matchingItemIndex = searchResults.findIndex(item => item.imdbID === likedButtonMediaId);
         // Toggle liked state of object in searchResults array
         searchResults[matchingItemIndex].liked = !searchResults[matchingItemIndex].liked;
-        console.log(searchResults[matchingItemIndex]);
         // Update watchlist variable with a class instance from searchResults that matches the index of media item that was liked
         watchlist = searchResults[matchingItemIndex].liked ? addToStoredWatchlist(searchResults[matchingItemIndex], watchlist) 
                         : removeFromStoredWatchlist(searchResults[matchingItemIndex], watchlist);
