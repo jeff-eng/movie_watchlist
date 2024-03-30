dc9f838099d6b454bd7140a9409827e458a9b715
jeff-eng <me@jeffeng.com>
2024-03-29 18:29:41 -0700

    Added section for when watchlist is empty. Included text to inform user.

--- js/watchlist-functions.js
+++ js/watchlist-functions.js
@@ -40,18 +40,17 @@ function removeFromStoredWatchlist(mediaItem, movieWatchlist) {
 }
 
 function renderWatchlistHtml(watchlist) {
-    // console.log(Object.values(watchlist));
     return Object.values(watchlist).map(item => item.createResultHtml());
 }
 
 function renderWatchlist(watchlist) {    
     const watchlistSectionEl = document.getElementById('watchlist');
-    
-    if (watchlist) {
+
+    if (Object.keys(watchlist).length) {
         watchlistSectionEl.replaceChildren(...renderWatchlistHtml(watchlist));
     } else {
         // Render an empty watchlist message
-        watchlistSectionEl.innerHTML = '<h2>Time to add stuff to your watchlist</h2>';
+        document.getElementById('empty-list').classList.remove('hide');
     }
 }
 
